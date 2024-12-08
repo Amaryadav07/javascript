1689fd2e22e5518aaed0042d560377359571ba9a
Amaryadav07 <anath2893007@gmail.com>
2024-12-08 21:26:28 +0530

    added if else

--- main.js
+++ main.js
@@ -14,63 +14,63 @@ function abc() {
 // }
 
 // multiple if and else if
-// let a=50;
-// let b=100;
+let a=50;
+let b=100;
 
-// if (confirm("please add the value of a and b")) {
+if (confirm("please add the value of a and b")) {
     
   
-//     console.log(a+b);
-// }
+    console.log(a+b);
+}
 
-// else if (confirm("please multiply the value of a and b")) {
-//      console.log(a*b);
+else if (confirm("please multiply the value of a and b")) {
+     console.log(a*b);
      
-// }
-// else if (confirm("please subtract the value of a and b")) {
-//     console.log(a-b);
+}
+else if (confirm("please subtract the value of a and b")) {
+    console.log(a-b);
     
-// }
-// else if (confirm("please modulus the value of a and b")) {
-//     console.log(a%b);
+}
+else if (confirm("please modulus the value of a and b")) {
+    console.log(a%b);
     
-// }
-// else if (confirm("please divide the value of a and b")) {
-//     console.log(a/b);
+}
+else if (confirm("please divide the value of a and b")) {
+    console.log(a/b);
     
-// }
-// else{
-//    alert("you have choosen no action")
-// }
+}
+else{
+   alert("you have choosen no action")
+}
 
 // marks of students
-let name = window.prompt("Enter your name:"); 
-let marks = window.prompt("Enter your marks out of 100:");
+// let name = window.prompt("Enter your name:"); 
+// let marks = window.prompt("Enter your marks out of 100:");
 
 
-marks = parseInt(marks);
+// marks = parseInt(marks);
 
 
-let result = "";  
-    if (marks >= 90 && marks<=100) 
-        {
-        result = `Congratulations ${name}! You scored ${marks} and received an A+ grade.`;
-    } 
-    else if (marks >= 80) {
-        result = `Well done ${name}! You scored ${marks} and received an A grade.`;
-    }
-     else if (marks >= 70) {
-        result = `Good job ${name}! You scored ${marks} and received a B grade.`;
-    }
-     else if (marks >= 60) {
-        result = `Nice try ${name}. You scored ${marks} and received a C grade.`;
-    } 
-    else if (marks >= 50) {
-        result = `You passed ${name} with a score of ${marks}, and received a D grade.`;
-    } 
-    else {
-        result = `Sorry ${name}, you failed with a score of ${marks}. Better luck next time.`;
-    }
-    window.alert(result);
+// let result = "";  
+//     if (marks >= 90 && marks<=100) 
+//         {
+//         result = `Congratulations ${name}! You scored ${marks} and received an A+ grade.`;
+//     } 
+//     else if (marks >= 80) {
+//         result = `Well done ${name}! You scored ${marks} and received an A grade.`;
+//     }
+//      else if (marks >= 70) {
+//         result = `Good job ${name}! You scored ${marks} and received a B grade.`;
+//     }
+//      else if (marks >= 60) {
+//         result = `Nice try ${name}. You scored ${marks} and received a C grade.`;
+//     } 
+//     else if (marks >= 50) {
+//         result = `You passed ${name} with a score of ${marks}, and received a D grade.`;
+//     } 
+//     else {
+//         result = `Sorry ${name}, you failed with a score of ${marks}. Better luck next time.`;
+//     }
+//     window.alert(result);
 }
 abc();
