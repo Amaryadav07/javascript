fd374ff80b6045b272b024f6683c4050cab3b413
Amaryadav07 <anath2893007@gmail.com>
2024-12-09 22:55:23 +0530

    added code

---
+++ task.js
@@ -0,0 +1,122 @@
+function task() {
+    // question 1. ask the user of their age and if it is greater than or equal to 18 with ternary operator and display the message in alert that they are eligible to vote and not eligible.
+
+   
+    // let age=prompt("enter your age");
+    // (age>=18)? alert("you are eligible"):alert("you are not eligible")
+// ----------------------------------------------------------------------------------------------------------------------------
+//  question-2
+
+//   let course=confirm("do you completed your course")
+//   let certificate=confirm("do you have certificate")
+//   if (course==true && certificate==true) {
+
+
+//     alert("completed")
+//   }
+
+//   else{
+//     alert("not completed")
+//   }
+//  --------------------------------------------------------------------------------------------------------------------------------
+//    question-3
+
+// let price=prompt("enter price of an item");
+// let discount=confirm("do you have discount coupon");
+
+// if (discount==true) {
+//     confirm("yes i have 10% of discount")
+//     alert(price*0.9)
+// }
+// else{
+//     alert("no discount")
+// }
+// --------------------------------------------------------------------------------------------------------------------------------
+// question-4
+//  let marks= prompt("enter your marks")
+//  if (marks>=33) {
+//     alert("congratulations you have passed")
+    
+//  }
+//  else {
+//     alert("you have failed the exam ")
+//  }
+// --------------------------------------------------------------------------------------------------------------------
+// question-5
+//  let number= prompt("enter your number")
+//  if (number%2==0) {
+//     alert("even number")
+    
+//  }
+//  else {
+//     alert("odd number")
+//  }
+// -------------------------------------------------------------------------------------------------------------------
+// question-6
+
+// let a= confirm("are you sure you want to delete")
+
+// if (a==true) {
+//     alert("Item Deleted")
+    
+// }
+// else{
+//     alert("Cancelled")
+// }
+// ----------------------------------------------------------------------------------------------------------------------
+// question-7
+// let number=prompt("Enter number")
+// if (number>=0) {
+//    alert("positive number") 
+// }
+// else{
+//     alert("negative number")
+// }
+// --------------------------------------------------------------------------------------------------------------------
+//question-8
+
+// let i=prompt("enter your numbers to display")
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+    
+//   }
+  // ---------------------------------------------------------------------------------------------------------------
+  // question-9
+  let num;
+
+  do{
+    num=prompt("enter your number")
+  }
+  while(num<10)
+    alert("welcome user ")
+  // -------------------------------------------------------------------------------------------------------------------
+  // question-10
+ let day= parseInt(prompt("enter the day of the week")) ;
+ switch (day) {
+    case 1:
+    alert("today is monday")
+    break;
+    case 2:
+    alert("today is tuesday")
+    break;
+    case 3:
+    alert("today is wednesday")
+    break;
+    case 4:
+    alert("today is thrusday")
+    break;
+    case 5:
+    alert("today is friday")
+    break;
+    case 6:
+    alert("today is saturday")
+    break;
+    case 7:
+    alert("today is sunday")
+    break;
+    default:
+    alert("enter valid day")
+    break;
+ }
+}
+task();
