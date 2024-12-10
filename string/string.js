04bedd129b7c94f261b6f35e8b84e040d529ae1e
Amaryadav07 <anath2893007@gmail.com>
2024-12-10 14:07:30 +0530

    added string methods

---
+++ string/string.js
@@ -0,0 +1,12 @@
+function string() {
+
+    let abc="hello world";
+    let def="i am amar"
+    // document.write(abc.length) //length of the string
+    // document.write(abc.slice(2,8)) // slice the length with given start indexing no and ending indexing number
+    // document.write(abc.concat(def)) // add two string values together
+    // document.write(def.toUpperCase(0,9)) // it will convert the text in capitals letter
+    // document.write(def.toLowerCase(0,9)) // it will convert the text in smaller letter
+    
+}
+string()
